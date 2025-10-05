fc4cc7b4a9d4af5c37cb96ef310240775ecf444a
Mihai Ursu <ursu.mihai.mail@gmail.com>
2025-10-05 20:33:12 +0300

    added wrangler

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,17 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@astrojs/react';
+import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
+  adapter: cloudflare({
+    platformProxy: {
+      enabled: true
+    },
+
+    imageService: "cloudflare"
+  }),
   integrations: [react(), ],
   vite: {
     plugins: [tailwindcss()],
